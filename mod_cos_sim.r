798bd798283e1805a90840d8c0b402b213308fe0
Peter Csizsek <peter.csizsek@gmail.com>
2015-09-22 21:58:11 +0200

    parameter optimization :(

--- mod_cos_sim.r
+++ mod_cos_sim.r
@@ -28,36 +28,48 @@ cplte$VALIDPERIOD <- as.factor(cplte$VALIDPERIOD)
 
 # sets up sum of coupon USABLE_DATEs for training and test dataset
 for (i in 12:20) {
-    cpltr[is.na(cpltr[,i]),i] <- 0;    cpltr[cpltr[,i]>1,i] <- 1
-    cplte[is.na(cplte[,i]),i] <- 0;    cplte[cplte[,i]>1,i] <- 1
+    cpltr[is.na(cpltr[,i]),i] <- 0
+    cpltr[cpltr[,i]>1,i] <- 1
+    cplte[is.na(cplte[,i]),i] <- 0
+    cplte[cplte[,i]>1,i] <- 1
 }
 cpltr$USABLE_DATE_sum <- rowSums(cpltr[,12:20])
 cplte$USABLE_DATE_sum <- rowSums(cplte[,12:20])
 
 # start train set by merging coupon_detail_train and coupon_list_train
 # to get USER_ID_hash by coupon
+cols <- c("COUPON_ID_hash",
+        "USER_ID_hash",
+        "GENRE_NAME",
+        "DISCOUNT_PRICE",
+        "DISPPERIOD",
+        "large_area_name",
+        "small_area_name",
+        "VALIDPERIOD",
+        "USABLE_DATE_sum")
+
 train <- merge(cpdtr,cpltr)
-train <- train[,c("COUPON_ID_hash","USER_ID_hash",
-                  "GENRE_NAME","DISCOUNT_PRICE","DISPPERIOD",
-                  "large_area_name","small_area_name","VALIDPERIOD","USABLE_DATE_sum")]
+train <- train[,cols]
+
 # append test set to the training set for model.matrix factor column conversion
 cplte$USER_ID_hash <- "dummyuser"
-cpchar <- cplte[,c("COUPON_ID_hash","USER_ID_hash",
-                   "GENRE_NAME","DISCOUNT_PRICE","DISPPERIOD",
-                   "large_area_name","small_area_name","VALIDPERIOD","USABLE_DATE_sum")]
+cpchar <- cplte[,cols]
 train <- rbind(train,cpchar)
 
 #NA imputation to values of 1
 train[is.na(train)] <- 1
+
 #feature engineering
 train$DISCOUNT_PRICE <- 1/log10(train$DISCOUNT_PRICE)    
-train$DISPPERIOD[train$DISPPERIOD>7] <- 7;train$DISPPERIOD <- train$DISPPERIOD/7
+train$DISPPERIOD[train$DISPPERIOD>7] <- 7
+train$DISPPERIOD <- train$DISPPERIOD/7
 train$USABLE_DATE_sum <- train$USABLE_DATE_sum/9
 
 #convert the factors to columns of 0's and 1's
 train <- cbind(train[,c(1,2)],model.matrix(~ -1 + .,train[,-c(1,2)],
                                            contrasts.arg=lapply(train[,names(which(sapply(train[,-c(1,2)], is.factor)==TRUE))], 
                                                                 contrasts, contrasts=FALSE)))
+
 #separate the test from train following factor column conversion
 test <- train[train$USER_ID_hash=="dummyuser",]
 test <- test[,-2]
@@ -77,7 +89,7 @@ names(uchar) <- names(train)[2:dim(train)[2]]
 uchar <- aggregate(.~USER_ID_hash, data=rbind(uchar,train[,-1]),FUN=sum)
 
 # Add visit training data in chunks due to large dataset
-imax <- dim(cpvtr)[1]   
+imax <- dim(cpvtr)[1]
 i2 <- 1
 while (i2 < imax) {  # this loop takes a few minutes      
     i1 <- i2
@@ -85,16 +97,19 @@ while (i2 < imax) {  # this loop takes a few minutes
     if (i2 > imax) i2 <- imax
     cat("Merging coupon visit data i1=",i1," i2=",i2,"\n")
     trainv <- merge(cpvtr[i1:i2,],cpltr, by.x="VIEW_COUPON_ID_hash", by.y="COUPON_ID_hash")
-    trainv <- trainv[,c("VIEW_COUPON_ID_hash","USER_ID_hash",
-                        "GENRE_NAME","DISCOUNT_PRICE","DISPPERIOD",                          
-                        "large_area_name","small_area_name","VALIDPERIOD","USABLE_DATE_sum")]
+    cols[1] <- "VIEW_COUPON_ID_hash"
+    trainv <- trainv[,cols]
+    
     #same treatment as with coupon_detail train data
-    trainv$DISCOUNT_PRICE <- 1;train$DISPPERIOD <- 1;train$USABLE_DATE_sum <- 1;trainv[is.na(trainv)] <- 1
+    trainv$DISCOUNT_PRICE <- 1
+    train$DISPPERIOD <- 1
+    train$USABLE_DATE_sum <- 1
+    trainv[is.na(trainv)] <- 1
     trainv <- cbind(trainv[,c(1,2)],model.matrix(~ -1 + .,trainv[,-c(1,2)],
                                                  contrasts.arg=lapply(trainv[,names(which(sapply(trainv[,-c(1,2)], is.factor)==TRUE))], 
                                                                       contrasts, contrasts=FALSE)))
     # discount coupon visits relative to coupon purchases
-    couponVisitFactor <- .005      
+    couponVisitFactor <- .00813      
     trainv[,3:dim(trainv)[2]] <- trainv[,3:dim(trainv)[2]] * couponVisitFactor  
     uchar <- aggregate(.~USER_ID_hash, data=rbind(uchar,trainv[,-1]),FUN=sum)    
 }
@@ -102,8 +117,8 @@ while (i2 < imax) {  # this loop takes a few minutes
 # Weight matrix with 7 factors, separate for male and female users 
 #Weight Matrix: GENRE_NAME, DISCOUNT_PRICE, DISPPERIOD, large_area_name, small_area_name, VALIDPERIOD, USABLE_DATE_sum
 require(Matrix)
-weightm <- c(1.962, 1.25, 1.25, 1.00, 4.50, 0.625, 0.4) # males weights
-weightf <- c(1.75, 0.75, 1.50, 1.00, 4.50, 0.625, 0.25) # female weights
+weightm <- c(1.962, 1.25, 1.25, 1.00, 6.50, 0.625, 0.4) # males weights
+weightf <- c(1.75, 0.75, 1.50, 1.00, 6.50, 0.625, 0.25) # female weights
 Wm <- as.matrix(Diagonal(x=c(rep(weightm[1],13), rep(weightm[2],1), rep(weightm[3],1), rep(weightm[4],9), 
                              rep(weightm[5],55),rep(weightm[6],2),rep(weightm[7],1))))
 Wf <- as.matrix(Diagonal(x=c(rep(weightf[1],13), rep(weightf[2],1), rep(weightf[3],1), rep(weightf[4],9), 
@@ -112,11 +127,13 @@ Wf <- as.matrix(Diagonal(x=c(rep(weightf[1],13), rep(weightf[2],1), rep(weightf[
 #calculation of cosine similarities of users and coupons
 score = as.matrix(uchar[,2:ncol(uchar)]) %*% Wm %*% t(as.matrix(test[,2:ncol(test)]))
 score[ulist$SEX_ID=='f',] = as.matrix(uchar[ulist$SEX_ID=='f',2:ncol(uchar)]) %*% Wf %*% t(as.matrix(test[,2:ncol(test)]))
+
 #order the list of coupons according to similairties and take only first 10 coupons
 uchar$PURCHASED_COUPONS <- do.call(rbind, lapply(1:nrow(uchar),FUN=function(i){
     purchased_cp <- paste(test$COUPON_ID_hash[order(score[i,], decreasing = TRUE)][1:10],collapse=" ")
     return(purchased_cp)
 }))
+
 #make submission
 submission <- uchar[,c("USER_ID_hash","PURCHASED_COUPONS")]
 submission$PURCHASED_COUPONS[rowSums(score)==0] <- ""
